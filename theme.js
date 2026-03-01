481e3edf9f5b18c27a7d4db5f0b34bd5eee6e895
EclipsedOlm <skywingkq25@gmail.com>
2026-03-01 10:49:13 +0800

    revamping website design, wip

---
+++ theme.js
@@ -0,0 +1,30 @@
+if(localStorage.getItem("theme") === null){
+    localStorage.setItem("theme", "default");
+}
+
+const stylesheet = document.documentElement.style;
+const themes = new Map([
+    ["default", ["#ffffaa", "#ffffee"]],
+    ["new", ["#2be41a", "#ffffee"]],
+]);
+
+function themeRefresh(){
+
+    let currTheme = localStorage.getItem("theme");
+    
+    if(themes.has(currTheme) === false){
+        localStorage.setItem("theme", "default");
+        currTheme = localStorage.getItem("theme");
+    }
+
+    stylesheet.setProperty("--primary", themes.get(currTheme)[0]);
+    stylesheet.setProperty("--secondary", themes.get(currTheme)[1]);
+
+}
+
+function setTheme(newTheme = "default"){
+
+    localStorage.setItem("theme", newTheme);
+    themeRefresh();
+
+}
